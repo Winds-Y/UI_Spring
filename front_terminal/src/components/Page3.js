d7aef2cc04d88014529e92ee65b30b0a52f060dd
yangtse <2155513297@qq.com>
2019-05-15 13:57:49 +0800

    add routes

---
+++ front_terminal/src/components/Page3.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Page3 extends React.Component{
+    render(){
+        return (
+            <div>
+                <div style={{
+                    fontSize:'16px',
+                    textAlign:'center'
+                }}>This is Page3</div>
+            </div>
+        );
+    }
+}
+
+export default Page3;
--- front_terminal/src/index.js
+++ front_terminal/src/index.js
@@ -4,8 +4,12 @@ import './index.css';
 import App from './App';
 import UserList from './components/UserList';
 import {BrowserRouter, Route} from 'react-router-dom'
+import Home from './components/Home'
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css'
+import Page1 from './components/Page1'
+import Page2 from './components/Page2'
+import Page3 from './components/Page3'
 
 // ReactDOM.render(
 //     (<BrowserRouter>
@@ -14,7 +18,21 @@ import 'bootstrap/dist/css/bootstrap.css'
 //         </div>
 //     </BrowserRouter>),
 //     document.getElementById('root'));
-ReactDOM.render(<UserList/>,document.getElementById('root'));
+// ReactDOM.render(<Home/>,document.getElementById('root'));
+ReactDOM.render(
+    (
+        <BrowserRouter>
+            <div>
+                <Route path={'/'} component={Home}/>
+                <Route path={'/Page1'} component={Page1}/>
+                <Route path={'/Page2'} component={Page2}/>
+                <Route path={'/Page3'} component={Page3}/>
+                <Route path={'/UserList'} component={UserList}/>
+            </div>
+        </BrowserRouter>
+    ),
+    document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
